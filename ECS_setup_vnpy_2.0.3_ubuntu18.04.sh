7b5b61f47c1bf16c1ef2faf75da91dcf6d43ff91
listplot3d <xiao.k.li@gmail.com>
2019-06-01 13:56:41 +0800

    Update ECS_setup_vnpy_2.0.3_ubuntu18.04.sh

--- ECS_setup_vnpy_2.0.3_ubuntu18.04.sh
+++ ECS_setup_vnpy_2.0.3_ubuntu18.04.sh
@@ -93,8 +93,13 @@ echo '
 from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
-from vnpy.gateway.ctp import CtpGateway
+from vnpy.gateway.jyt import JYTGateway
+
 from vnpy.app.cta_strategy import CtaStrategyApp
+from vnpy.app.data_recorder import DataRecorderApp
+from vnpy.app.algo_trading import AlgoTradingApp
+from vnpy.app.csv_loader import CsvLoaderApp
+from vnpy.app.cta_backtester import CtaBacktesterApp
 
 def main():
 
@@ -103,8 +108,12 @@ def main():
     event_engine = EventEngine()
     main_engine = MainEngine(event_engine)
     
-    main_engine.add_gateway(CtpGateway)
+    main_engine.add_gateway(JYTGateway)
+    main_engine.add_app(AlgoTradingApp)
+    main_engine.add_app(CsvLoaderApp)
+    main_engine.add_app(CtaBacktesterApp)
     main_engine.add_app(CtaStrategyApp)
+    main_engine.add_app(DataRecorderApp)
 
     main_window = MainWindow(main_engine, event_engine)
     main_window.showMaximized()
